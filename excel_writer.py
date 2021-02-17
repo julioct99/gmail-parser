75d87425433b8dea6afdfde93b41d1a8245c828e
julioct99 <jcasallot01@gmail.com>
2021-02-17 18:53:24 +0100

    Modify to write just one excel workbook

--- excel_writer.py
+++ excel_writer.py
@@ -1,25 +1,51 @@
-from constants import IMG_PROPERTY, EXCEL_FOLDER
+from constants import IMG_PROPERTY, EXCEL_FOLDER, EXCEL_WORKBOOK
 import xlsxwriter
+import openpyxl
+import os
 
 
 class ExcelWriter:
     def __init__(self, dictionaries):
-        self.route = EXCEL_FOLDER
+        self.workbook = EXCEL_WORKBOOK
         self.dictionaries = dictionaries
 
     def write_excel_files(self):
-        for dictionary in self.dictionaries:
-            workbook = xlsxwriter.Workbook(f"{self.route}/{dictionary['dni']}.xlsx")
-            worksheet = workbook.add_worksheet()
-            self.write_worksheet(worksheet, dictionary)
-            workbook.close()
+        if not os.path.exists(self.workbook):
+            self.create_workbook()
 
-    def write_worksheet(self, worksheet, dictionary):
-        row, col = 0, 0
+        workbook = openpyxl.load_workbook(filename=self.workbook)
+        worksheet = workbook['Sheet1']
+        if self.dictionaries:
+            self.write_headers(worksheet)
+            row = self.get_last_row() if os.path.exists(self.workbook) else 1
+            for dictionary in self.dictionaries:
+                self.write_to_worksheet(worksheet, row, dictionary)
+                row += 1
+            workbook.save(self.workbook)
+
+    def create_workbook(self):
+        workbook = xlsxwriter.Workbook(self.workbook)
+        worksheet = workbook.add_worksheet()
+        workbook.close()
+
+    def write_headers(self, worksheet):
+        col = 1
+        headers = self.dictionaries[0].keys()
+        for header in headers:
+            worksheet.cell(row=1, column=col, value=header.upper())
+            col += 1
+
+    def get_last_row(self):
+        workbook = openpyxl.load_workbook(filename=self.workbook)
+        worksheet = workbook['Sheet1']
+        return worksheet.max_row + 1
+
+    def write_to_worksheet(self, worksheet, row, dictionary):
+        col = 1
         for key, value in dictionary.items():
-            worksheet.write(row, col, key)
             if key == IMG_PROPERTY:
-                worksheet.write_url(row, col + 1, value)
+                worksheet.cell(row=row, column=col).hyperlink = value
             else:
-                worksheet.write(row, col + 1, value)
-            row += 1
+                worksheet.cell(row=row, column=col, value=value)
+            col+=1
+            
